af5352bae1ed55bdf5fcd4bd99c6f54ef1299921
Atharva Jadhav <atharvavjadhav21@gmail.com>
2021-10-05 13:43:44 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Choose Option :\n1. New Game\n2. Quit");
+                
+                int choice;
+                if (int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.Clear();
+                    if (choice == 1)
+                    {
+                        TicTacToe ticTacToe = new TicTacToe();
+                        Console.WriteLine("Player 1 choose your character. Press x / o ::");
+                        string playerOneCharacter = Console.ReadLine();
+                        bool isGameSet = ticTacToe.SetPlayerCharacters(playerOneCharacter);
+                        if (isGameSet)
+                        {
+                            string currentGameStatus = "PLAYING";
+                            while (currentGameStatus.Equals("PLAYING") )
+                            {
+                                ticTacToe.ShowCurrentGame();
+                                Console.WriteLine("====================================");
+                                Console.WriteLine("Player 1 - Enter where you want to place the character. ");
+                                Console.Write("Avaiable Places =>  ");
+                                ticTacToe.ShowAvaiblePlaces();
+                                
+                                int place;
+                                if (int.TryParse( Console.ReadLine(),out place))
+                                {
+                                    ticTacToe.SetCharInPlace();
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Invalid Place was entered");
+                                }
+                                //Last Line
+                                currentGameStatus = ticTacToe.CheckForWin();
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid Character choosen x_x");
+                        }
+
+                    }
+                    else if (choice == 2)
+                    {
+                        Console.WriteLine("Thank you for playing.");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid choice. Press Enter to continue..");
+                        Console.ReadKey();
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid choice. Press Enter to continue..");
+                    Console.ReadKey();
+                    
+
+                }
+            }
+        }
+    }
+}
---
+++ TicTacToe.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class TicTacToe
+    {
+        private string[,] TictactoeArr;
+        private int[] availablePlaces;           
+
+        public string PlayerOneCharacter { get; set; }
+        public string PlayerTwoCharacter { get; set; }
+
+        public TicTacToe()
+        {
+            this.TictactoeArr = new string[3, 3] {
+                { "-", "-", "-" },
+                { "-", "-", "-" },
+                { "-", "-", "-" }
+            };
+            availablePlaces = new int[9] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        }
+
+        public string CheckForWin()
+        {
+            if (false)
+            {
+                // TODO: Implement the logic here
+                return "PLA";
+            }
+                return "PLAYING";
+        }
+
+        internal bool SetPlayerCharacters(string playerOneCharacter)
+        {
+            playerOneCharacter = playerOneCharacter.ToLower();
+            if (playerOneCharacter.Equals("x"))
+            {
+                PlayerOneCharacter = "x";
+                PlayerTwoCharacter = "o";
+                return true;
+
+            }
+            else if (playerOneCharacter.Equals("o"))
+            {
+                PlayerOneCharacter = "o";
+                PlayerTwoCharacter = "x";
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        internal void ShowCurrentGame()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(TictactoeArr[i, j] + "    ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private int[] GetAvaiablePlaces()
+        {
+            return availablePlaces;
+        }
+
+        public void ShowAvaiblePlaces()
+        {
+            Console.Write("[  ");
+            foreach (int avlPlace in GetAvaiablePlaces())
+            {
+                if (avlPlace != -1)
+                {
+                    Console.Write( avlPlace + "  ");
+                }
+            }
+            Console.Write("]");
+            Console.WriteLine();
+        }
+
+        public void SetCharInPlace()
+        {
+
+        }
+
+    }
+}
